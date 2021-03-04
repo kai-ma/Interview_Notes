cd1a53396db31848e5759d54aa063c85f060f7b8
mkx <mkx4499@163.com>
2021-03-04 14:00:09 +0800

    update xmind

---
+++ src/javabasic/concurrency/SyncDemo.java
@@ -0,0 +1,125 @@
+package javabasic.concurrency;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 验证synchronized四种使用方式：
+ * 1. 同步代码块(synchronized(this|object))
+ * 2. 同步非静态方法
+ * 3. 同步类(synchronized(类.class)
+ * 4. 同步静态方法
+ * 同步类作用在class对象上，同步对象作用在对象上，这是两种不同的锁，两者之间互相不影响 。
+ */
+public class SyncDemo implements Runnable {
+    @Override
+    public void run() {
+        String threadName = Thread.currentThread().getName();
+        if (threadName.startsWith("A")) {
+            async();
+        } else if (threadName.startsWith("B")) {
+            syncObjectBlock1();
+        } else if (threadName.startsWith("C")) {
+            syncObjectMethod1();
+        } else if (threadName.startsWith("D")) {
+            syncClassBlock1();
+        } else if (threadName.startsWith("E")) {
+            syncClassMethod1();
+        }
+
+    }
+
+    /**
+     * 异步方法
+     */
+    private void async() {
+        try {
+            System.out.println(Thread.currentThread().getName() + "_Async_Start: " + new SimpleDateFormat("HH:mm:ss").format(new Date()));
+            Thread.sleep(1000);
+            System.out.println(Thread.currentThread().getName() + "_Async_End: " + new SimpleDateFormat("HH:mm:ss").format(new Date()));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 方法中有 synchronized(this|object) {} 同步代码块
+     */
+    private void syncObjectBlock1() {
+        System.out.println(Thread.currentThread().getName() + "_SyncObjectBlock1: " + new SimpleDateFormat("HH:mm:ss").format(new Date()));
+        synchronized (this) {
+            try {
+                System.out.println(Thread.currentThread().getName() + "_SyncObjectBlock1_Start: " + new SimpleDateFormat("HH:mm:ss").format(new Date()));
+                Thread.sleep(1000);
+                System.out.println(Thread.currentThread().getName() + "_SyncObjectBlock1_End: " + new SimpleDateFormat("HH:mm:ss").format(new Date()));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * synchronized 修饰非静态方法
+     */
+    private synchronized void syncObjectMethod1() {
+        System.out.println(Thread.currentThread().getName() + "_SyncObjectMethod1: " + new SimpleDateFormat("HH:mm:ss").format(new Date()));
+        try {
+            System.out.println(Thread.currentThread().getName() + "_SyncObjectMethod1_Start: " + new SimpleDateFormat("HH:mm:ss").format(new Date()));
+            Thread.sleep(1000);
+            System.out.println(Thread.currentThread().getName() + "_SyncObjectMethod1_End: " + new SimpleDateFormat("HH:mm:ss").format(new Date()));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void syncClassBlock1() {
+        System.out.println(Thread.currentThread().getName() + "_SyncClassBlock1: " + new SimpleDateFormat("HH:mm:ss").format(new Date()));
+        synchronized (SyncDemo.class) {
+            try {
+                System.out.println(Thread.currentThread().getName() + "_SyncClassBlock1_Start: " + new SimpleDateFormat("HH:mm:ss").format(new Date()));
+                Thread.sleep(1000);
+                System.out.println(Thread.currentThread().getName() + "_SyncClassBlock1_End: " + new SimpleDateFormat("HH:mm:ss").format(new Date()));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private synchronized static void syncClassMethod1() {
+        System.out.println(Thread.currentThread().getName() + "_SyncClassMethod1: " + new SimpleDateFormat("HH:mm:ss").format(new Date()));
+        try {
+            System.out.println(Thread.currentThread().getName() + "_SyncClassMethod1_Start: " + new SimpleDateFormat("HH:mm:ss").format(new Date()));
+            Thread.sleep(1000);
+            System.out.println(Thread.currentThread().getName() + "_SyncClassMethod1_End: " + new SimpleDateFormat("HH:mm:ss").format(new Date()));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        SyncDemo syncThread = new SyncDemo();
+        Thread A_thread1 = new Thread(syncThread, "A_thread1");
+        Thread A_thread2 = new Thread(syncThread, "A_thread2");
+        Thread B_thread1 = new Thread(syncThread, "B_thread1");
+        Thread B_thread2 = new Thread(syncThread, "B_thread2");
+        Thread C_thread1 = new Thread(syncThread, "C_thread1");
+        Thread C_thread2 = new Thread(syncThread, "C_thread2");
+        Thread D_thread1 = new Thread(syncThread, "D_thread1");
+        Thread D_thread2 = new Thread(syncThread, "D_thread2");
+        Thread E_thread1 = new Thread(syncThread, "E_thread1");
+        Thread E_thread2 = new Thread(syncThread, "E_thread2");
+        A_thread1.start();
+        A_thread2.start();
+        B_thread1.start();
+        B_thread2.start();
+        C_thread1.start();
+        C_thread2.start();
+        D_thread1.start();
+        D_thread2.start();
+        E_thread1.start();
+        E_thread2.start();
+    }
+}
+
+
+
